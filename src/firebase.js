06899abf77528f94833f75bb7cd50fbab9fa1884
yorifuji <yorifuji@gmail.com>
2020-08-15 23:39:47 +0900

    change firebase config and deploy host

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import 'firebase/auth';
 import 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAFQZlG-4wo0UwapHt4DvawBG35NJcojh0",
-  authDomain: "react-firebase-1a904.firebaseapp.com",
-  databaseURL: "https://react-firebase-1a904.firebaseio.com",
-  projectId: "react-firebase-1a904",
-  storageBucket: "react-firebase-1a904.appspot.com",
-  messagingSenderId: "511194967473",
-  appId: "1:511194967473:web:c07f008441e261009aa190",
-  measurementId: "G-PCHDEMF2BD"
+  apiKey: "AIzaSyARmas2oqs3fEKWCTXXqhGFQiLqzXni8iY",
+  authDomain: "yorifuji-react-firebase-chat.firebaseapp.com",
+  databaseURL: "https://yorifuji-react-firebase-chat.firebaseio.com",
+  projectId: "yorifuji-react-firebase-chat",
+  storageBucket: "yorifuji-react-firebase-chat.appspot.com",
+  messagingSenderId: "386795881064",
+  appId: "1:386795881064:web:36b31b78ffba4b59c5b1dd",
+  measurementId: "G-9HF38PVFYD"
 };
 
 firebase.initializeApp(firebaseConfig);

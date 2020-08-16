e294ba5dc624df161617269d9f6ad74670d292c7
yorifuji <yorifuji@gmail.com>
2020-08-16 18:02:47 +0900

    staging environment.
    yarn firebase projects:list
    yarn firebase use yorifuji-react-firebase-chat-s
    yarn firebase deploy

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import 'firebase/auth';
 import 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyARmas2oqs3fEKWCTXXqhGFQiLqzXni8iY",
-  authDomain: "yorifuji-react-firebase-chat.firebaseapp.com",
-  databaseURL: "https://yorifuji-react-firebase-chat.firebaseio.com",
-  projectId: "yorifuji-react-firebase-chat",
-  storageBucket: "yorifuji-react-firebase-chat.appspot.com",
-  messagingSenderId: "386795881064",
-  appId: "1:386795881064:web:36b31b78ffba4b59c5b1dd",
-  measurementId: "G-9HF38PVFYD"
+  apiKey: "AIzaSyDz1OZ73Qx1_m00SVzlNqhh7cjYMHWSajk",
+  authDomain: "yorifuji-react-firebase-chat-s.firebaseapp.com",
+  databaseURL: "https://yorifuji-react-firebase-chat-s.firebaseio.com",
+  projectId: "yorifuji-react-firebase-chat-s",
+  storageBucket: "yorifuji-react-firebase-chat-s.appspot.com",
+  messagingSenderId: "1064988512443",
+  appId: "1:1064988512443:web:ebdead0f7d15299dd4199e",
+  measurementId: "G-LF4F2KFCFC"
 };
 
 firebase.initializeApp(firebaseConfig);

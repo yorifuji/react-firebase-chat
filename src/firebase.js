e51ec14cc96520c4ee06cbe2844d2de36eb66d0b
yorifuji <yorifuji@gmail.com>
2021-09-04 12:58:50 +0900

    firebaseConfigを変更

--- src/firebase.js
+++ src/firebase.js
@@ -4,31 +4,15 @@ import 'firebase/auth';
 import 'firebase/firestore'
 import 'firebase/analytics'
 
-// production
 const firebaseConfig = {
-  apiKey: "AIzaSyARmas2oqs3fEKWCTXXqhGFQiLqzXni8iY",
-  authDomain: "yorifuji-react-firebase-chat.firebaseapp.com",
-  databaseURL: "https://yorifuji-react-firebase-chat.firebaseio.com",
-  projectId: "yorifuji-react-firebase-chat",
-  storageBucket: "yorifuji-react-firebase-chat.appspot.com",
-  messagingSenderId: "386795881064",
-  appId: "1:386795881064:web:36b31b78ffba4b59c5b1dd",
-  measurementId: "G-9HF38PVFYD"
+  apiKey: "AIzaSyBswItMjX0cuhf7c0IBNuKIY6LQ8dkRTEY",
+  authDomain: "react-firebase-chat-demo-32d02.firebaseapp.com",
+  projectId: "react-firebase-chat-demo-32d02",
+  storageBucket: "react-firebase-chat-demo-32d02.appspot.com",
+  messagingSenderId: "810042434665",
+  appId: "1:810042434665:web:50564f4098e7c12b7f2ef8",
+  measurementId: "G-6E1FRQMJKP"
 };
-
-/*
-// staging
-const firebaseConfig = {
-  apiKey: "AIzaSyDz1OZ73Qx1_m00SVzlNqhh7cjYMHWSajk",
-  authDomain: "yorifuji-react-firebase-chat-s.firebaseapp.com",
-  databaseURL: "https://yorifuji-react-firebase-chat-s.firebaseio.com",
-  projectId: "yorifuji-react-firebase-chat-s",
-  storageBucket: "yorifuji-react-firebase-chat-s.appspot.com",
-  messagingSenderId: "1064988512443",
-  appId: "1:1064988512443:web:ebdead0f7d15299dd4199e",
-  measurementId: "G-LF4F2KFCFC"
-};
-*/
 
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
